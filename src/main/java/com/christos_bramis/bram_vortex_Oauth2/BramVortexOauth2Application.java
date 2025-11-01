6a7e1efee11c365652dab82791988a30d3db435a
Christos Bramis <bramis04chris@gmail.com>
2025-11-01 20:48:49 +0200

    Repo init

---
+++ src/main/java/com/christos_bramis/bram_vortex_Oauth2/BramVortexOauth2Application.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex_Oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BramVortexOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BramVortexOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/christos_bramis/bram_vortex_Oauth2/BramVortexOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex_Oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BramVortexOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
