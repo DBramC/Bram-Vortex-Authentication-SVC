af26cce38307bbe595b0b300685b08a6a60571dc
Christos Bramis <bramis04chris@gmail.com>
2025-11-04 14:48:45 +0200

    Added the Jwt to the security filter chain

--- src/main/java/com/christos_bramis/bram_vortex_Oauth2/config/Security.java
+++ src/main/java/com/christos_bramis/bram_vortex_Oauth2/config/Security.java
@@ -1,9 +1,10 @@
 package com.christos_bramis.bram_vortex_Oauth2.config;
 
 
+import com.christos_bramis.bram_vortex_Oauth2.GitHubJwtSuccessHandler;
+import com.christos_bramis.bram_vortex_Oauth2.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,12 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class Security {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.
-                authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2Login(oauth2 ->
+                        oauth2.successHandler(new GitHubJwtSuccessHandler(jwtTokenProvider))
+                );
 
         return http.build();
     }
